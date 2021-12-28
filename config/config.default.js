5c8a11bec1c2089dc4dc60a1964db9652cfdcd46
zkboys <876213774@qq.com>
2021-12-28 12:05:43 +0800

    :sparkles: swagger

--- config/config.default.js
+++ config/config.default.js
@@ -4,4 +4,36 @@
  * egg-decorator-router default config
  * @member Config#routeDecorator
  */
-exports.routeDecorator = {}
+exports.routeDecorator = {
+    swagger: {
+        basePath: '/',
+        apiInfo: {
+            title: 'egg-swagger',
+            description: 'swagger-ui for egg js api',
+            version: '1.0.0'
+        },
+        schemes: ['http', 'https'],
+        consumes: ['application/json'],
+        produces: ['application/json'],
+        securityDefinitions: {
+            // apikey: {
+            //   type: 'apiKey',
+            //   name: 'clientkey',
+            //   in: 'header',
+            // },
+            // oauth2: {
+            //   type: 'oauth2',
+            //   tokenUrl: 'http://petstore.swagger.io/oauth/dialog',
+            //   flow: 'password',
+            //   scopes: {
+            //     'write:access_token': 'write access_token',
+            //     'read:access_token': 'read access_token',
+            //   },
+            // },
+        },
+        enableSecurity: false,
+        // enableValidate: true,
+        routerMap: false,
+        enable: true
+    }
+}
